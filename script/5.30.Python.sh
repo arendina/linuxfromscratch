5d9225ec0c4f2261ea0cc827a023a395d9b137af
Antonio Rendina <antonio.rendina@bluemetrix.com>
2019-06-11 16:06:59 +0200

    fixed error on script

--- script/5.30.Python.sh
+++ script/5.30.Python.sh
@@ -2,14 +2,13 @@
 # 5.30. Perl-$ver
 set -e
 
-ver='3.7,2'
+ver='3.7.2'
 
 cd $LFS/sources
 tar -xvf Python-3.7.2.tar.xz
 cd Python-$ver
 
-sed -i '/def add_multiarch_paths/a \
-return' setup.py
+sed -i '/def add_multiarch_paths/a \        return' setup.py
 
 ./configure --prefix=/tools --without-ensurepip
 
